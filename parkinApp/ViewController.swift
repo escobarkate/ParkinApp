0f5a53b9519693da9672ed7ab7288b0b50a5b00e
Mario Gironza Cerón <mariogironza@MacBook-Air-de-Mario.local>
2016-11-14 22:56:33 -0500

    Copia 1 Parking App

---
+++ parkinApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parkinApp
+//
+//  Created by Mario Gironza Cerón on 14/11/16.
+//  Copyright © 2016 movil unicauca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
