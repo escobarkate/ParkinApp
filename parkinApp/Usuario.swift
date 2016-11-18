0886aac6a93da47d1450585e166a154fd9dad182
Mario Gironza Cerón <mariogironza@MacBook-Air-de-Mario.local>
2016-11-18 12:47:23 -0500

    prueba backend

---
+++ parkinApp/Usuario.swift
@@ -0,0 +1,15 @@
+//
+//  Usuario.swift
+//  parkinApp
+//
+//  Created by Mario Gironza Cerón on 17/11/16.
+//  Copyright © 2016 movil unicauca. All rights reserved.
+//
+
+import Foundation
+
+class Usuario {
+    var nombre: String!
+    var user: String!
+    var password: String!
+}
--- parkinApp/ViewController.swift
+++ parkinApp/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var nombreUsuario: UITextField!
+    @IBOutlet var nombreUsuario: UITextField!
     
-    @IBOutlet weak var contraseña: UITextField!
+    @IBOutlet var contraseña: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +25,69 @@ class ViewController: UIViewController {
     }
 
     @IBAction func goToTabs(_ sender: Any) {
-        performSegue(withIdentifier: "next", sender: nil)    }
+        let client:HttpClient = HttpClient()
+        let usu = nombreUsuario.text!
+        let contra = contraseña.text!
+        
+        let jsonUser = "{\"user\":\""+usu+"\",\"password\":\""+contra+"\"}"
+        client.post(url: "http://10.130.9.70:8080/usuarios/login/", json: jsonUser, callback: processData)
+        
+           }
 
     
     @IBAction func goToRegistrar(_ sender: Any) {
         performSegue(withIdentifier: "reg", sender: nil)     }
+    
+    
+    func processData(data:Data?){
+        
+        do{
+            let json:NSDictionary = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSDictionary
+            
+            let success:Bool = json.value(forKey: "success") as! Bool
+            if success == true {
+                //let usuario:NSDictionary = json.object(forKey: "user") as! NSDictionary
+                //let nombres:NSDictionary = usuario.object(forKey: "nombres") as! NSDictionary
+                //let usr:NSDictionary = usuario.object(forKey: "user") as! NSDictionary
+                
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "next", sender: nil)
+                }
+                
+            }
+            
+        }catch{}
+        
+        
+        
+    }
+    
+    func JSONStringify(value: AnyObject,prettyPrinted:Bool = false) -> String{
+        
+        let options = prettyPrinted ? JSONSerialization.WritingOptions.prettyPrinted : JSONSerialization.WritingOptions(rawValue: 0)
+        
+        
+        if JSONSerialization.isValidJSONObject(value) {
+            
+            do{
+                let data = try JSONSerialization.data(withJSONObject: value, options: options)
+                if let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                    return string as String
+                }
+            }catch {
+                
+                print("error")
+                //Access error here
+            }
+            
+        }
+        return ""
+        
+    }
+
+
+
+
+
 }
 
